9029afadaaf802cef659f1d2ea62cfdb4d555e21
Lupanova Anna <hanna.lupanava@newland.by>
2023-11-01 15:22:33 +0300

    add docker

--- market_place/market_place/celery.py
+++ market_place/market_place/celery.py
@@ -2,8 +2,10 @@ import os
 from celery import Celery, shared_task
 from celery.schedules import crontab
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'market_place.settings')
-app = Celery('market_place')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app = Celery('market_place', broker='redis://redis:6379', backend='redis://redis:6379')
+app.conf.broker_url = 'redis://redis:6379/0'
+
+app.config_from_object('django.conf:settings')
 
 app.autodiscover_tasks()
 
@@ -18,24 +20,6 @@ app.conf.beat_schedule = {
     }
 
 }
-
-
-
-
-#
-# @app.on_after_configure.connect
-# def setup_periodic_tasks(sender, **kwargs):
-#     # Calls test('hello') every 10 seconds.
-#     sender.add_periodic_task(10.0, task_one.s, name='add every 10')
-#     #
-#     # # Calls test('world') every 30 seconds
-#     # sender.add_periodic_task(30.0, test.s('world'), expires=10)
-#     #
-#     # # Executes every Monday morning at 7:30 a.m.
-#     # sender.add_periodic_task(
-#     #     crontab(hour=7, minute=30, day_of_week=1),
-#     #     test.s('Happy Mondays!'),
-#     # )
 
 
 
